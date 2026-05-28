ca137d89bdf8339de030d66c77eb12f8afaf55f8
mondalGourish <gourishmondal02@gmail.com>
2026-05-28 20:27:30 +0530

    done the about and product component

--- src/landing_page/products/Hero.js
+++ src/landing_page/products/Hero.js
@@ -1,7 +1,14 @@
 import React from 'react';
 function Hero() {
     return ( 
-        <h1>Hero</h1>
+        <div className='container border-bottom'>
+            <div className='text-center mt-5 p-3'>
+                <h2>TradeSphere Products</h2>
+                <h4 className='text-muted mt-3 fs-4'>Sleek, modern, and intuitive trading platforms</h4 >
+                <p className='mt-3 mb-5'>Check out our <a href="" style={{textDecoration: "none"}}> investment offerings<i class="fa-solid fa-arrow-right-long"></i></a></p>
+            </div>
+
+        </div>
      );
 }
 
